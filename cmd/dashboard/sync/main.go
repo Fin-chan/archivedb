a46f19071e540fcaada3d3fcd76e1825b9a02c1b
jialei <jialeicui@126.com>
2022-05-02 20:42:41 +0800

    bk

--- cmd/dashboard/sync/main.go
+++ cmd/dashboard/sync/main.go
@@ -5,7 +5,10 @@ import (
 	"io/ioutil"
 	"os"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"gopkg.in/yaml.v3"
+
+	"github.com/jialeicui/archivedb/pkg"
 )
 
 type Config struct {
@@ -13,12 +16,12 @@ type Config struct {
 	Cookie string `yaml:"cookie"`
 }
 
-func main() {
-	configPath := ".config.yaml"
-	if len(os.Args) > 1 {
-		configPath = os.Args[1]
-	}
+const (
+	configPath = ".config.yaml"
+	dbPath     = ".data"
+)
 
+func main() {
 	content, err := ioutil.ReadFile(configPath)
 	if err != nil {
 		panic(err)
@@ -29,15 +32,40 @@ func main() {
 		panic(err)
 	}
 
+	os.RemoveAll(dbPath)
+	db, err := pkg.New(dbPath)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
 	cli, err := NewWithHeader(map[string]string{"cookie": config.Cookie})
 	if err != nil {
 		panic(err)
 	}
 
-	url := fmt.Sprintf("https://weibo.com/ajax/favorites/all_fav?uid=%s&page=1", config.Uid)
+	url := fmt.Sprintf("https://weibo.com/ajax/favorites/all_fav?uid=%s&page=2", config.Uid)
 	resp, err := cli.Get(url)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(resp))
+
+	item := pkg.Item{}
+	err = bson.UnmarshalExtJSON(resp, true, &item)
+	if err != nil {
+		panic(err)
+	}
+	ok := item["ok"]
+	if ok != int32(1) {
+		panic(fmt.Sprintf("invalid content: %q", string(resp)))
+	}
+	for _, i := range item["data"].(bson.A) {
+		it := i.(bson.M)
+		err = db.Put(&it,
+			pkg.WithKey([]byte(it["idstr"].(string))),
+		)
+		if err != nil {
+			panic(err)
+		}
+	}
 }
